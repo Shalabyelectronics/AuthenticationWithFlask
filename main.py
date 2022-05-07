b6c65e860fd667d72a5308371f9c0a9a5602bdcc
shalabyelectronics <mhdshalabystar@gmail.com>
2022-05-07 11:42:24 +0300

    add next to page after login

--- main.py
+++ main.py
@@ -60,20 +60,25 @@ def download():
     return send_from_directory('static/files', 'cheat_sheet.pdf')
 
 
+global next_page
+
+
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    global next_page
     if current_user.is_authenticated:
         return redirect(url_for("secrets"))
+    if request.method == "GET":
+        next_page = request.args.get("next")
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
         if user and check_password_hash(user.password, password):
             login_user(user)
-            next_page = request.args.get("next")
-            print(next_page)
             return redirect(next_page) if next_page else redirect(url_for('secrets'))
-        flash("Login unsuccessful. please check email and password", "danger")
+        else:
+            flash("Login unsuccessful. please check email and password", "danger")
     return render_template("login.html")
 
 
